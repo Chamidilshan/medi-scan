fef3e4c8dd42bcf370489fb279ee781a70390ee0
Chamika Dilshan <chamidilshan55@gmail.com>
2025-01-31 09:08:58 +0530

    fixed deployment issues

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/medi-scan/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
